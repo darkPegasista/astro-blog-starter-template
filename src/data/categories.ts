4a138fc1917bcefb1aea78e9c1f148f5788920cb
Dash <165929103+darkPegasista@users.noreply.github.com>
2026-08-28 15:31:59 +0200

    Create categories.ts

---
+++ src/data/categories.ts
@@ -0,0 +1,29 @@
+export const categories = [
+	{
+		slug: 'bottle-openers',
+		name: 'Bottle Openers',
+		description: 'Fun designs for your next drink.',
+		image: '/placeholder-product.svg',
+	},
+
+	{
+		slug: 'bucket-hats',
+		name: 'Bucket Hats',
+		description: 'Wearable designs featuring my artwork.',
+		image: '/placeholder-product.svg',
+	},
+
+	{
+		slug: 'posters',
+		name: 'Outdoor Posters',
+		description: 'Artwork made to be displayed outdoors.',
+		image: '/placeholder-product.svg',
+	},
+
+	{
+		slug: 'stickers',
+		name: 'Stickers',
+		description: 'Small pieces of artwork for your stuff.',
+		image: '/placeholder-product.svg',
+	},
+];
