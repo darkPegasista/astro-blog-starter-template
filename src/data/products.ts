63032de314d7741ef65043cb827d53ad9056d997
Dash <165929103+darkPegasista@users.noreply.github.com>
2026-08-28 15:09:54 +0200

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,1 +1,82 @@
+export const products = [
+	{
+		slug: 'wine-pony-cheers',
+		name: 'Wine Pony Cheers',
+		category: 'Bottle Opener',
+		price: 10,
+		stock: 1,
+		image: '/images/merch/placeholder-product.svg',
+	},
 
+	{
+		slug: 'dont-drink-and-fly',
+		name: "Don't Drink and Fly",
+		category: 'Bottle Opener',
+		price: 10,
+		stock: 1,
+		image: '/images/merch/placeholder-product.svg',
+	},
+
+	{
+		slug: 'pony-princesses',
+		name: 'Pony Princesses',
+		category: 'Bucket Hat',
+		price: 35,
+		stock: 1,
+		image: '/images/merch/placeholder-product.svg',
+	},
+
+	{
+		slug: 'six-best-friends',
+		name: 'Six Best Friends',
+		category: 'Bucket Hat',
+		price: 35,
+		stock: 1,
+		image: '/images/merch/placeholder-product.svg',
+	},
+
+	{
+		slug: 'musician-ponies',
+		name: 'Musician Ponies',
+		category: 'Bucket Hat',
+		price: 35,
+		stock: 1,
+		image: '/images/merch/placeholder-product.svg',
+	},
+
+	{
+		slug: 'villains-of-harmony',
+		name: 'Villains of Harmony',
+		category: 'Bucket Hat',
+		price: 35,
+		stock: 1,
+		image: '/images/merch/placeholder-product.svg',
+	},
+
+	{
+		slug: 'socialist-pony-propaganda',
+		name: 'Socialist Pony Propaganda',
+		category: 'Outdoor Poster',
+		price: 20,
+		stock: 1,
+		image: '/images/merch/placeholder-product.svg',
+	},
+
+	{
+		slug: 'drama-unicorn',
+		name: 'Drama Unicorn',
+		category: 'Sticker',
+		price: 3,
+		stock: 1,
+		image: '/images/merch/placeholder-product.svg',
+	},
+
+	{
+		slug: 'cider-dash',
+		name: 'Cider Dash',
+		category: 'Sticker',
+		price: 3,
+		stock: 1,
+		image: '/images/merch/placeholder-product.svg',
+	},
+];
