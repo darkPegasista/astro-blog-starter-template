921290fdadeb0e57f12fc2077fc856bc498aa5f6
Dash <165929103+darkPegasista@users.noreply.github.com>
2026-09-03 19:15:59 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+wrangler.jsonc
+
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
